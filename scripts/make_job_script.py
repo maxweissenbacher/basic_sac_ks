4af97976ff3615831239c9915f0792bad3a32ea3
max <k00rdel@gmail.com>
2024-08-21 09:51:38 +0000

    modified standard config settings

--- scripts/make_job_script.py
+++ scripts/make_job_script.py
@@ -1,3 +1,5 @@
+def make_yaml(params):
+    yaml_content = f"""
 apiVersion: batch/v1
 kind: Job
 metadata:
@@ -5,8 +7,8 @@ metadata:
  labels:
    kueue.x-k8s.io/queue-name: eidf079ns-user-queue
 spec:
- completions: 5
- parallelism: 5
+ completions: 1
+ parallelism: 1
  template:
   spec:
    restartPolicy: Never
@@ -28,7 +30,7 @@ spec:
          git checkout main;
          pip install -r requirements_ks.txt;
          wandb login ac8ec66b318e6624089b2723c3174f01a850416c;
-         python main_sac.py env.nu=0.005 logger.project_name=SAC_KS_basic_algo
+         python main_sac.py env.nu=0.04 logger.project_name=SAC_KS_gridsearch_UTDratio env.exp_name=KS_utd_ratio_{params["utd"]} optim.utd_ratio={params["utd"]} collector.frames_per_batch=500;
      resources:
       requests:
        cpu: 8
@@ -43,3 +45,31 @@ spec:
    - name: dshm
      emptyDir:
       medium: Memory
+
+"""
+    return yaml_content
+
+if __name__ == '__main__':
+    import numpy as np
+
+    def gamma_to_horizon(x):
+        return int(1/(1-x))
+    
+    def horizon_to_gamma(x):
+        return 1-1/x
+
+    # horizon_values = [2, 5, 10, 20, 50, 100, 200, 500]
+    # frames_per_batch = [50, 100, 200, 500, 1000, 5000]
+    utd_ratios = [0.5, 0.8, 1.0, 1.2, 1.5, 1.8, 2.0, 2.5, 3.0, 5.0]
+
+    counter = 0
+    for j, v in enumerate(utd_ratios):
+        params = {}
+        # params['gamma'] = horizon_to_gamma(v)
+        # params['fpb'] = v
+        params['utd'] = v
+
+        with open(f'utd_ratio_scripts/job_{counter}.yaml', 'w') as file:
+            file.write(make_yaml(params))
+
+        counter += 1
