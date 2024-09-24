79cb1f1a7c420bb5ccf1c1dc26e0922d6adadcca
maxweissenbacher <max.weissenbacher@proton.me>
2024-09-24 16:54:19 +0100

    changed performance plotting

--- utils/plot_performance.py
+++ utils/plot_performance.py
@@ -1,45 +1,56 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import wandb
-import pandas as pd
 
 
-def load_runs_from_wandb_project(path, algorithm):
+def load_runs_from_wandb_project_SAC(path):
     api = wandb.Api()
-    df = pd.DataFrame()
-    for run in api.runs(path=path):
+    run = api.run(path)
+    if not run.state == "finished":
+        raise ValueError(f"Run with ID {run.id} is not finished. Skipping this run.")
+    nu = eval(run.config['env'])['nu']
+    num_envs = eval(run.config['logger'])['num_eval_envs']
+    all_rewards = []
+    for env_id in range(num_envs):
         rewards = []
-        last_rewards = []
-        cae_errors = []
-        cae_absolute_errors = []
+        for i, row in run.history(keys=[f"eval/reward_{env_id}"]).iterrows():
+            rewards.append(row[f"eval/reward_{env_id}"])
+        all_rewards.append(rewards)
+
+    return nu, np.asarray(all_rewards)
+
+
+def load_runs_from_wandb_project_DREAMER(path):
+    print(f"WARNING: we normalise the Dreamer data by dividing by sqrt(64)=8. This should probably be fixed. BEWARE")
+    norm = np.sqrt(64)
+    api = wandb.Api()
+    run = api.run(path)
+    if not run.state == "finished":
+        raise ValueError(f"Run with ID {run.id} is not finished. Skipping this run.")
+    nu = run.config["KS"]["nu"]
+    num_envs = run.config["envs"]["amount"]
+    all_rewards = []
+    for env_id in range(num_envs):
+        rewards = []
+        for i, row in run.history(keys=[f"rollout_eval_episode/rollout_reward_{env_id}"]).iterrows():
+            rewards.append(row[f"rollout_eval_episode/rollout_reward_{env_id}"])
+        all_rewards.append(rewards)
+
+    return nu, np.asarray(all_rewards)/norm
+
+
+def intersperse_lists(list1, list2):
+    return [item for pair in zip(list1, list2) for item in pair]
+
+
+def load_config_from_wandb_project(path):
+    api = wandb.Api(timeout=30)
+    for run in api.runs(path=path):
         if not run.state == "finished":
             print(f"Run with ID {run.id} is not finished. Skipping this run.")
             continue
-        if not algorithm == run.name[:3]:
-            continue
-        num_sensors = eval(run.config['env'])['num_sensors']
-        nu = eval(run.config['env'])['nu']
-        num_actuators = eval(run.config['env'])['num_actuators']
-        num_envs = eval(run.config['env'])['num_envs']
-        use_cae = True if eval(run.config['env'])['path_to_cae_model'] else False
-        for i, row in run.history(keys=["eval/reward"]).iterrows():
-            rewards.append(row["eval/reward"])
-        for i, row in run.history(keys=["eval/last_reward"]).iterrows():
-            last_rewards.append(row["eval/last_reward"])
-        if use_cae:
-            for i, row in run.history(keys=["eval/cae_relative_L2_error"]).iterrows():
-                cae_errors.append(row["eval/cae_relative_L2_error"])
-            for i, row in run.history(keys=["eval/cae_absolute_L2_error"]).iterrows():
-                cae_absolute_errors.append(row["eval/cae_absolute_L2_error"])
-        else:
-            cae_errors = len(rewards) * [0.]
-            cae_absolute_errors = len(rewards) * [0.]
-        df[run.name, use_cae, 'reward'] = rewards
-        df[run.name, use_cae, 'errors'] = cae_errors
-        df[run.name, use_cae, 'abs_errors'] = cae_absolute_errors
-        #df[run.id, use_cae, 'last_reward'] = last_rewards
-
-    return df
+        return run.config
+    return None
 
 
 if __name__ == '__main__':
@@ -48,44 +59,28 @@ if __name__ == '__main__':
         "text.usetex": True,
         "font.family": "Helvetica"
     })
-    no_memory_color = 'xkcd:cornflower blue'
-    attention_memory_color = 'xkcd:coral'
 
     # ---------------------------
     # Data loading
     # ---------------------------
 
-    # Load metrics for attention from WandB
-    project_name = "LatentRL_solarsweep1"
-    df_list = {}
-    df_list['SAC'] = load_runs_from_wandb_project(
-        path="why_are_all_the_good_names_taken_aaa/" + project_name,
-        algorithm='SAC'
-    )
-    df_list['PPO'] = load_runs_from_wandb_project(
-        path="why_are_all_the_good_names_taken_aaa/" + project_name,
-        algorithm='PPO'
-    )
+    # Load metrics from WandB
+    team_name = "why_are_all_the_good_names_taken_aaa"
+    project_name = "basic_sac_trained_on_L_12_eval_on_L_12"
+    d_sac = {}
+    d_sac['train L12 eval L60'] = load_runs_from_wandb_project_SAC(path='/'.join([team_name, project_name, '6ed1flhe']))
+    d_sac['train L12 eval L31'] = load_runs_from_wandb_project_SAC(path='/'.join([team_name, project_name, '3khujipp']))
+    d_sac['train L12 eval L22'] = load_runs_from_wandb_project_SAC(path='/'.join([team_name, project_name, 'w1wee5dz']))
+    d_sac['train L12 eval L18'] = load_runs_from_wandb_project_SAC(path='/'.join([team_name, project_name, 'rb3tgm4q']))
+    d_sac['train L12 eval L12'] = load_runs_from_wandb_project_SAC(path='/'.join([team_name, project_name, 'bta2goy5']))
 
-    df_list_errors = {}
-    df_list_absolute_errors = {}
-    for key, df in df_list.items():
-        df_cae = df[[c for c in df.columns if c[1] and c[2] == 'reward']]
-        df_no_cae = df[[c for c in df.columns if not c[1] and c[2] == 'reward']]
-        df_list[key] = {'CAE': df_cae, 'NO CAE': df_no_cae}
-        df_list_errors[key] = df[[c for c in df.columns if c[1] and c[2] == 'errors']]
-        df_list_absolute_errors[key] = df[[c for c in df.columns if c[1] and c[2] == 'abs_errors']]
-
-    # Interpolate the errors to be the same length
-    max_len = max([len(d) for d in df_list_errors.values()])
-    for key, df in df_list_errors.items():
-        if len(df) < max_len:
-            x = np.linspace(0, 1, num=max_len)
-            xp = np.linspace(0, 1, num=len(df))
-            new_df = pd.DataFrame()
-            for col in df.columns:
-                new_df[col] = np.interp(x, xp, df[col])
-            df_list_errors[key] = new_df
+    project_name = "Dreamer_KS_generalizability"
+    d_dreamer = {}
+    d_dreamer['train L12 eval L60'] = load_runs_from_wandb_project_DREAMER(path='/'.join([team_name, project_name, 'ugtni2p7']))
+    d_dreamer['train L12 eval L31'] = load_runs_from_wandb_project_DREAMER(path='/'.join([team_name, project_name, '6jkfrbdm']))
+    d_dreamer['train L12 eval L22'] = load_runs_from_wandb_project_DREAMER(path='/'.join([team_name, project_name, 'uoq3aiiz']))
+    d_dreamer['train L12 eval L18'] = load_runs_from_wandb_project_DREAMER(path='/'.join([team_name, project_name, 'nytomo85']))
+    d_dreamer['train L12 eval L12'] = load_runs_from_wandb_project_DREAMER(path='/'.join([team_name, project_name, 'ctmx5x3l']))
 
     # ---------------------------
     # Plotting
@@ -93,97 +88,100 @@ if __name__ == '__main__':
     plt.rcParams.update({
         "text.usetex": True,
         "font.family": "Helvetica",
-        'font.size': '20',
+        'font.size': '15',
     })
 
-    # Plot performance as a function of time
-    algorithm = 'PPO'
-    color1 = 'xkcd:cornflower blue'
-    color2 = 'xkcd:coral'
-    colors = {'CAE': color1, 'NO CAE': color2}
-    fig, ax = plt.subplots(figsize=(12, 6))
-    for key, metric in df_list[algorithm].items():
-        ax.plot(metric.abs().mean(axis=1), label=key, color=colors[key])
+    last_rewards_sac = {}
+    last_rewards_dreamer = {}
+    mean_rewards_sac = {}
+    mean_rewards_dreamer = {}
 
-        ax.fill_between(
-            range(len(metric.mean(axis=1))),
-            metric.abs().mean(axis=1) - 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().min(axis=1),
-            metric.abs().mean(axis=1) + 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().max(axis=1),
-            color=colors[key], alpha=.2)
+    for key, (nu, rewards) in d_sac.items():
+        fig, ax = plt.subplots(figsize=(10, 5))
+        for env_id in range(rewards.shape[0]):
+            plt.plot(rewards[env_id, :], label=f'env {env_id}')
+        #plt.legend(loc='center right', bbox_to_anchor=(1, 0.5))
+        plt.xlabel('Environment steps')
+        plt.ylabel('Energy')
+        plt.title(f'nu={nu}')
+        plt.savefig(f'SAC {key}.png')
+        plt.close()
 
-        #ax.plot(metric.abs(), color=colors[model], alpha=0.25)
+        last_rewards_sac[str(int(2*np.pi/np.sqrt(nu)))] = rewards[:, -1]
+        mean_rewards_sac[str(int(2 * np.pi / np.sqrt(nu)))] = np.mean(rewards, axis=-1)
 
-    """
-    ax.set_xticks(
-        range(0, len(metric.min(axis=1)), 4),
-        [f'{(x + 1) * 25}M' for x in range(0, len(metric.min(axis=1)), 4)]
+    for key, (nu, rewards) in d_dreamer.items():
+        # temporary fix for the 0 value
+        rewards = rewards[:, 1:]
+        # remove this once fixed
+        fig, ax = plt.subplots(figsize=(10, 5))
+        for env_id in range(rewards.shape[0]):
+            plt.plot(rewards[env_id, :], label=f'env {env_id}')
+        #plt.legend(loc='center right', bbox_to_anchor=(1, 0.5))
+        plt.xlabel('Environment steps')
+        plt.ylabel('Energy')
+        plt.title(f'nu={nu}')
+        plt.savefig(f'Dreamer {key}.png')
+        plt.close()
+
+        last_rewards_dreamer[str(int(2*np.pi/np.sqrt(nu)))] = rewards[:, -1]
+        mean_rewards_dreamer[str(int(2 * np.pi / np.sqrt(nu)))] = np.mean(rewards, axis=-1)
+
+    # Plot boxplots for last rewards
+    fig, ax = plt.subplots(figsize=(10, 5))
+    plt_list = []
+    lbl_list = []
+    for key in last_rewards_sac.keys():
+        plt_list.append(np.abs(last_rewards_sac[key]))
+        plt_list.append(np.abs(last_rewards_dreamer[key]))
+        lbl_list.append(f"{key}")
+        lbl_list.append(f"{key}")
+    box = plt.boxplot(
+        plt_list,
+        labels=lbl_list,
+        patch_artist=True,
     )
-    """
-    ax.set_xlabel('Solver steps')
-    ax.set_ylabel("Energy " + r"$\displaystyle E$")
-    # ax.set_ylabel('$\displaystyle L^2$ norm')
-    ax.set_yscale('log')
-    # plt.title(f"$\displaystyle L^2$ norm of KS solution per solver timestep")
-    plt.legend()
-    # plt.savefig('l2norm_filled.png', dpi=300, bbox_inches='tight', format='png')
+    # Color the boxes: red for sac, blue for dreamer
+    colors = ['red', 'blue']
+    for i, boxplot in enumerate(box['boxes']):
+        boxplot.set_facecolor(colors[i % 2])
+        boxplot.set_alpha(0.5)
+
+    plt.yscale('log')
+    plt.ylabel('Final reward')
+    plt.title('Transfer learning (trained on L=12.5) \n Red = SAC, Blue = Dreamer')
+    plt.xlabel('L (rounded to nearest integer)')
+    plt.savefig('boxplot_last_reward_trained_on_L12.png')
     plt.show()
+    plt.close()
 
-    # Plot CAE errors as a function of time
-    color1 = 'xkcd:cornflower blue'
-    color2 = 'xkcd:coral'
-    colors = {'SAC': color1, 'PPO': color2}
-    fig, ax = plt.subplots(figsize=(12, 6))
-    for key, metric in df_list_errors.items():
-        ax.plot(metric.abs().mean(axis=1), label=key, color=colors[key])
-
-        ax.fill_between(
-            range(len(metric.mean(axis=1))),
-            metric.abs().mean(axis=1) - 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().min(axis=1),
-            metric.abs().mean(axis=1) + 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().max(axis=1),
-            color=colors[key], alpha=.2)
-
-        # ax.plot(metric.abs(), color=colors[model], alpha=0.25)
-
-    """
-    ax.set_xticks(
-        range(0, len(metric.min(axis=1)), 4),
-        [f'{(x + 1) * 25}M' for x in range(0, len(metric.min(axis=1)), 4)]
+    # Plot boxplots for mean rewards
+    fig, ax = plt.subplots(figsize=(10, 5))
+    plt_list = []
+    lbl_list = []
+    for key in mean_rewards_sac.keys():
+        plt_list.append(np.abs(mean_rewards_sac[key]))
+        plt_list.append(np.abs(mean_rewards_dreamer[key]))
+        lbl_list.append(f"{key}")
+        lbl_list.append(f"{key}")
+    box = plt.boxplot(
+        plt_list,
+        labels=lbl_list,
+        patch_artist=True,
     )
-    """
-    ax.set_xlabel('Solver steps')
-    ax.set_ylabel("Relative " + r"$\displaystyle L^2$" + " error")
-    # ax.set_ylabel('$\displaystyle L^2$ norm')
-    # plt.title(f"$\displaystyle L^2$ norm of KS solution per solver timestep")
-    plt.legend()
-    # plt.savefig('l2norm_filled.png', dpi=300, bbox_inches='tight', format='png')
+    # Color the boxes: red for sac, blue for dreamer
+    colors = ['red', 'blue']
+    for i, boxplot in enumerate(box['boxes']):
+        boxplot.set_facecolor(colors[i % 2])
+        boxplot.set_alpha(0.5)
+
+    plt.yscale('log')
+    plt.ylabel('Mean episodic reward')
+    plt.title('Transfer learning (trained on L=12.5) \n Red = SAC, Blue = Dreamer')
+    plt.xlabel('L (rounded to nearest integer)')
+    plt.savefig('boxplot_mean_reward_trained_on_L12.png')
     plt.show()
+    plt.close()
 
-    # Plot CAE absolute errors as a function of time
-    color1 = 'xkcd:cornflower blue'
-    color2 = 'xkcd:coral'
-    colors = {'SAC': color1, 'PPO': color2}
-    fig, ax = plt.subplots(figsize=(12, 6))
-    for key, metric in df_list_absolute_errors.items():
-        ax.plot(metric.abs().mean(axis=1), label=key, color=colors[key])
 
-        ax.fill_between(
-            range(len(metric.mean(axis=1))),
-            metric.abs().mean(axis=1) - 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().min(axis=1),
-            metric.abs().mean(axis=1) + 1.96 * metric.abs().sem(axis=1),  # df_base_sens.abs().max(axis=1),
-            color=colors[key], alpha=.2)
 
-        # ax.plot(metric.abs(), color=colors[model], alpha=0.25)
-
-    """
-    ax.set_xticks(
-        range(0, len(metric.min(axis=1)), 4),
-        [f'{(x + 1) * 25}M' for x in range(0, len(metric.min(axis=1)), 4)]
-    )
-    """
-    ax.set_xlabel('Solver steps')
-    ax.set_ylabel("Absolute " + r"$\displaystyle L^2$" + " error")
-    # ax.set_ylabel('$\displaystyle L^2$ norm')
-    # plt.title(f"$\displaystyle L^2$ norm of KS solution per solver timestep")
-    plt.legend()
-    # plt.savefig('l2norm_filled.png', dpi=300, bbox_inches='tight', format='png')
-    plt.show()
